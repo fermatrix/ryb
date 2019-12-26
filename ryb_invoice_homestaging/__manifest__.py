7908534a1763e7136abd6b430c6d376052ed1d87
Fernando Plaza <fernando.plaza@enzo.es>
2019-12-26 13:07:42 +0100

    Nuevas imagenes

---
+++ ryb_invoice_homestaging/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ryb_inovice_homestaging",
+
+    'summary': """
+        Modelo de factura personalizada de Home Staging""",
+
+    'description': """
+        Modelo de factura personalizada de Home Staging
+    """,
+
+    'author': "FJMR",
+    'website': "https://www.enzo.es",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/report_homestaging.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
